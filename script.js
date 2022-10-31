4d68582aff5c81db06160513fca50719e99147e7
GithubCoder2021 <91566024+GithubCoder2021@users.noreply.github.com>
2022-10-31 09:14:18 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,23 @@
-var size = 10
-var growthPerSecond = 0
+var size = 10;
+var growthPerSecond = 0;
+var growthCost = 10;
 
 setInterval(function() {
   size += growthPerSecond;
 	
   document.getElementById("number").innerText = size;
 }, 1000)
+
+function buyGrowth() {
+   if (size >= growthCost) {
+      size-= growthCost;
+      growthPerSecond ++;
+      document.getElementById("number").innerText = size;
+      document.getElementById("growthSpeed").innerText = growthPerSecond;
+      growthCost = Math.floor(growthCost * 1.15);
+      document.getElementById("growthPurchase").innerText = growthCost;
+   }
+   else {
+     alert("You're too small!!!");
+       }  
+   }
