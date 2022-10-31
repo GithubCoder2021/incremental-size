e111d195a7577bed385b6bb2b12a349a614ce2e1
GithubCoder2021 <91566024+GithubCoder2021@users.noreply.github.com>
2022-10-31 12:31:16 -0400

    Update script.js

--- script.js
+++ script.js
@@ -30,7 +30,7 @@ function buyMulti() {
       document.getElementById("number").innerText = size;
       growthPerSecond = Math.floor(growthPerSecond * multiplier);
       document.getElementById("growthSpeed").innerText = growthPerSecond;
-      multiCost = Math.floor(multiCost * 1.63);
+      multiCost = Math.floor(multiCost * 1.45);
       document.getElementById("multiPurchase").innerText = multiCost;
    } else {
      alert("You're too small!!!");
