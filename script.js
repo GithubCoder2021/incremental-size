a8ce79dbbac2c74cd84cb47f11c74b0a6bc0f9bf
GithubCoder2021 <91566024+GithubCoder2021@users.noreply.github.com>
2022-10-31 11:37:17 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 var size = 10;
 var growthPerSecond = 0;
 var growthCost = 10;
-
+var multiCost = 100;
+var multiplier = 1.5;
 setInterval(function() {
   size += growthPerSecond;
 	
@@ -17,6 +18,20 @@ function buyGrowth() {
       document.getElementById("growthSpeed").innerText = growthPerSecond;
       growthCost = Math.floor(growthCost * 1.15);
       document.getElementById("growthPurchase").innerText = growthCost;
+   } else {
+     alert("You're too small!!!");
+    }  
+	
+}
+
+function buyMulti() { 
+   if (size >= multiCost) {
+      size -= multiCost;
+      document.getElementById("number").innerText = size;
+      multiplier = Math.floor(growthPerSecond * multiplier);
+      document.getElementById("multiplier").innerText = multiplier;
+      multiCost = Math.floor(multiCost * 1.2);
+      document.getElementById("multiCost").innerText = multicost;
    } else {
      alert("You're too small!!!");
     }  
